559d955cccf02ec027ae8f2b021f47a8968403b1
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-08-20 14:54:43 +0300

    05 Player 2 Has Joined the Game (players frontend design)

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -52,24 +52,49 @@ const fillSquare = (index) => {
 
 <template>
   <AuthenticatedLayout class="text-black dark:text-white">
-    <div class="">The Game Goes Here</div>
+    <main class="container mx-auto">
+      <div class="mt-8 flex justify-center text-2xl uppercase font-semibold">
+        The Game Goes Here
+      </div>
 
-    <menu class="mt-12 mx-auto grid grid-cols-3 gap-1.5 w-0 min-w-fit">
-      <li
-        v-for="(square, index) in boardState"
-        class="size-40 grid place-items-center bg-gray-300 dark:bg-gray-600"
-      >
-        <button
-          v-if="square === 0"
-          @click="fillSquare(index)"
-          class="bg-gray-200 dark:bg-gray-700 place-self-stretch hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors"
-        ></button>
-        <span
-          v-else
-          v-text="square === -1 ? 'X' : 'O'"
-          class="text-4xl font-bold"
-        ></span>
-      </li>
-    </menu>
+      <menu class="mt-12 mx-auto grid grid-cols-3 gap-1.5 w-0 min-w-fit">
+        <li
+          v-for="(square, index) in boardState"
+          class="size-40 grid place-items-center bg-gray-300 dark:bg-gray-600"
+        >
+          <button
+            v-if="square === 0"
+            @click="fillSquare(index)"
+            class="bg-gray-200 dark:bg-gray-700 place-self-stretch hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors"
+          ></button>
+          <span
+            v-else
+            v-text="square === -1 ? 'X' : 'O'"
+            class="text-4xl font-bold"
+          ></span>
+        </li>
+      </menu>
+
+      <ul class="max-w-sm mx-auto mt-6 space-y-2">
+        <li class="flex items-center gap-2">
+          <span
+            class="bg-gray-300 px-2 py-0.5 font-bold font-mono rounded dark:bg-gray-600"
+          >
+            X
+          </span>
+          <span>Test User</span>
+          <span class="bg-red-500 size-2 rounded-full"></span>
+        </li>
+        <li class="flex items-center gap-2">
+          <span
+            class="bg-gray-300 px-2 py-0.5 font-bold font-mono rounded dark:bg-gray-600"
+          >
+            O
+          </span>
+          <span>Ian</span>
+          <span class="bg-red-500 size-2 rounded-full"></span>
+        </li>
+      </ul>
+    </main>
   </AuthenticatedLayout>
 </template>
